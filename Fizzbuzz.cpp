4429af017d6ec42ddc20035cfe64735fc4742d53
Helpthispuplearrowpointsatme <114949941+Helpthispuplearrowpointsatme@users.noreply.github.com>
2025-10-16 17:37:36 +0200

    Create Fizzbuzz.cpp

---
+++ Fizzbuzz.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+#define give return
+#define ok }
+#define thx ;
+#define as {
+#define do {
+#define then {
+#define def
+#define please
+#define call
+#define with (
+#define thanks }
+#define thank );
+#define let int
+#define set
+#define output std::cout <<
+#define and &&
+#define not !
+#define or ||
+#define inc ++
+#define dec --
+#define equals ==
+
+def int test(int x, int y) as
+    please give 2 * x + y thx
+ok
+
+def int main() as
+    let x = call test with 10, 5 thank
+    please output x thx
+    for (let i = 0; i < 10; inc i) please do
+        output i thx
+    thanks
+    if (10 equals 10) then
+        output "10 equals 10" thx
+    ok
+    please give 0 thx
+ok
