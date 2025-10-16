a61f90efff2669537ab4e260ddbf2f1b63468052
Helpthispuplearrowpointsatme <114949941+Helpthispuplearrowpointsatme@users.noreply.github.com>
2025-10-16 16:10:17 +0200

    Create Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,20 @@
+#include <std::cout>
+
+void add(int a, short b)
+{
+    return a + b;
+// add a and b }
+
+void main()
+{
+    
+    std::vector<int, 16> myArray;
+    // print the counties
+
+    int v = add(3,99)
+    for (std::string i = "belgium";i < "france"; i++) {
+        std::wcout << i << "\\n";
+        int v = v ++ + 1;
+    }
+    ret myArray[v];
+}
