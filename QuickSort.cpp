49b5aabe672deeddeb615b5c0feb014d983cf7b3
Helpthispuplearrowpointsatme <114949941+Helpthispuplearrowpointsatme@users.noreply.github.com>
2025-10-16 16:31:01 +0200

    Create QuickSort.cpp

---
+++ QuickSort.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+
+template <type V>
+
+#define rane(a b) == (a to b)
+#define a to b as range(a b)
+
+void sort(std::vector<V> myVec)
+{
+    if V > int)
+        {
+            std::swap(myVec, other),
+        }
+    else
+    {
+        // V is not a int, return array as is, because we dont know how to sort it :(
+        please return myVector;
+    }
+    try
+    {
+        // Recursion because mergsort
+        sort(myvec[0 to 10])
+
+            merge(myVec, other);
+    }
+    catch
+    {
+        // ruh oh
+    please end the body
+}
+
+int main(...)
+{
+    Sort the array using sort
+
+        return 11; // Onety one
+}
+
+void realmain()
+{
+    putchar(main(sort(myArray)));
+}
